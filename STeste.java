34f9daa8af520d27cfbae53af9cddc72c63646dd
heniotierra <heniotster@gmail.com>
2016-08-24 21:48:32 -0300

    Add files via upload

---
+++ STeste.java
@@ -0,0 +1,33 @@
+import br.ufc.crateus.eda.st.STArrayList;
+
+public class STeste {
+
+	public static void main(String[] args) {
+		Integer n1 = new Integer("4");
+		Integer n2 = new Integer("3");
+		Integer n3 = new Integer("6");
+		Integer n4 = new Integer("6");
+		Integer n5 = new Integer("6");
+		Integer n6 = new Integer("6");
+		Integer n7 = new Integer("6");
+		Integer n8 = new Integer("6");
+		Integer n9 = new Integer("6");
+		Integer n10 = new Integer("6");
+		Integer n11 = new Integer("6");
+		STArrayList<Integer> list = new STArrayList<Integer>();
+		list.add(n1);
+		list.add(n2);
+		list.add(n3);
+		list.add(n4);
+		list.add(n5);
+		list.add(n6);
+		list.add(n7);
+		list.add(n8);
+		list.add(n9);
+		list.add(n10);
+		list.add(n11);
+		
+		
+	}
+
+}
